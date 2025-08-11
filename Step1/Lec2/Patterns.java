436a3086a7c9ab02efa5dc35e98ed781af6a3c59
Vinay Kumar J <jvkumar2003@gmail.com>
2025-08-11 21:47:18 +0530

    first commit

---
+++ Step1/Lec2/Patterns.java
@@ -0,0 +1,217 @@
+package Step1.Lec2;
+
+
+import java.util.Scanner;
+
+public class Patterns {
+
+    static void pattern1()
+    {
+
+        /*  This method is to print the below Pattern   */
+        
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("Enter Number to Print N*N Square Pattern : ");
+        int range = sc.nextInt();
+        System.out.println("");
+        System.out.println("Square Pattern ..");
+        System.out.println("");
+        for(int i = 1 ; i <= range ; i++)
+        {   
+            for(int j = 1 ; j <= range ; j++)
+            {
+                System.out.print(" *");
+            }  
+            System.out.println("");
+        }
+        System.out.println("");
+        sc.close();
+    }
+    static void pattern2()
+    {
+        Scanner sc  = new Scanner(System.in);
+        System.out.print("Enter the Range to Print The Pattern : ");
+        int range = sc.nextInt();
+        System.out.println("");
+        System.out.println("Ladder Pattern ..");
+        System.out.println("");
+        for(int i = 1 ; i <= range ; i++)
+        {
+            for(int j = 1 ; j <= i ; j ++)
+            {
+                System.out.print(" *");
+            }
+            System.out.println("");
+        }
+        System.out.println("");
+
+        sc.close();
+    }
+    static void pattern3()
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the Range to Print Pattern : ");
+        int range = sc.nextInt();
+        System.out.println();
+        System.out.println("Number Ladder Pattern ..");
+        System.out.println();
+        for(int i = 1; i <= range ; i++)
+        {
+            for(int j = 1 ; j <=i ; j++)
+            {
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+        sc.close();
+        System.out.println();
+    }
+    static void pattern4()
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the range to Print the Pattern : ");
+        int range = sc.nextInt();
+        System.out.println();
+        System.out.println("Number Pattern 2 ..");
+        System.out.println();
+
+        for(int i = 1 ; i <= range; i++)
+        {
+            for(int j = 1 ; j <= i ; j++)
+            {
+                System.out.print(i + " ");
+            }
+            System.out.println();
+        }
+        sc.close();
+        System.out.println();
+    }
+    static void pattern5()
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the range to Print the Pattern : ");
+        int range = sc.nextInt();
+        System.out.println();
+        System.out.println("Number Pattern 2 ..");
+        System.out.println();
+
+        for(int i = 1 ; i <= range; i++)
+        {
+            for(int j = 1 ; j <= ( range - i + 1) ; j++)
+            {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        sc.close();
+        System.out.println();
+    }
+    static void pattern6()
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the range to Print the Pattern : ");
+        int range = sc.nextInt();
+        System.out.println();
+        System.out.println("Number Pattern 2 ..");
+        System.out.println();
+
+        for(int i = 1 ; i <= range; i++)
+        {
+            for(int j = 1 ; j <= ( range - i + 1) ; j++)
+            {
+                System.out.print(j +" ");
+            }
+            System.out.println();
+        }
+        sc.close();
+        System.out.println();
+    }
+    public static void main(String[] args) 
+    {
+
+        /* 
+          Pattern 1           
+          
+          n * n Square with "*"
+          
+          * * * * *    
+          * * * * * 
+          * * * * *
+          * * * * * 
+          etc ..      
+          pattern1(); 
+        */
+      
+
+        /* 
+            Pattern 2           
+          
+            n * n  Ladder with "*"
+
+            *
+            * * 
+            * * * 
+            * * * * 
+            etc ..
+            pattern2();
+        */
+
+         /* 
+            Pattern 3           
+          
+            Number Ladder
+
+            1
+            1 2  
+            1 2 3 
+            1 2 3 4 
+            etc ..
+            pattern3();
+        */
+
+         /* 
+            Pattern 4           
+          
+            Number Ladder 2
+
+            1
+            2 2
+            3 3 3 
+            4 4 4 4  
+            etc ..
+            pattern4();
+        */
+
+        /* 
+            Pattern 5           
+          
+            Reverse Ladder with "*"
+            
+            * * * * *
+            * * * * 
+            * * *
+            * * 
+            *
+           
+            pattern5();
+        */
+
+         /* 
+            Pattern 6           
+          
+            Reverse Ladder with "*"
+            
+            1 2 3 4 5 
+            1 2 3 4
+            1 2 3 
+            1 2
+            1
+            
+            pattern6();
+        */
+        
+    }
+
+    
+}
