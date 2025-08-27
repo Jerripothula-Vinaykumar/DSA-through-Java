f387dfbb24531fc4f1890dd21e9f441e415d669b
Vinay Kumar J <jvkumar2003@gmail.com>
2025-08-27 10:13:04 +0530

    Basic Recursion  , 2 Problems Solved

---
+++ Step1/Lec5/OneToNUsingRecursion.java
@@ -0,0 +1,26 @@
+package Step1.Lec5;
+import java.util.Scanner;
+
+public class OneToNUsingRecursion {
+
+    public static void printOneToNUsingRecursion(int n )
+    {
+        if(n == 0)
+        {
+            return;
+        }
+        printOneToNUsingRecursion(n-1);
+        //Backtracking
+        System.out.println(n);
+
+    }
+
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Range to Print OneToN : ");
+        int n = sc.nextInt();
+        printOneToNUsingRecursion(n);
+        sc.close();
+    }
+}
